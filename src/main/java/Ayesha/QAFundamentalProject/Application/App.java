d7723d75f2b4b301d619bd43d56268406dbc3191
ayesharajaqa <75732244+ayesharajaqa@users.noreply.github.com>
2021-01-20 22:38:42 +0000

    added Root menu operations

--- src/main/java/Ayesha/QAFundamentalProject/Application/App.java
+++ src/main/java/Ayesha/QAFundamentalProject/Application/App.java
@@ -4,7 +4,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+import Templates.Customer;
+import Templates.Item;
+import Templates.Order;
+import Templates.Soap;
+import Templates.User;
+import Utilities.Database;
+import Utilities.ErrorHandler;
+import Utilities.Scan;
 
 public class App {
 	
@@ -15,7 +22,7 @@ public class App {
     	 */
     	
 		try {
-			//Instance of Database class
+			Database.connectToDatabase();
 		} catch (Exception exception) {
 			ErrorHandler.errorHandler(exception);
 		}
@@ -23,7 +30,7 @@ public class App {
 		/*
 		 * Create a new instance of the 'User' class.
 		 */
-    	
+    	User user= new User();
     	
     	/*
     	 * If the user isn't already logged in, then display the welcome menu.
@@ -48,7 +55,7 @@ public class App {
     			 */
     			case "1": 
     				
-    				//Create new user
+    				user = Controls.addNewUser(user);
     			
     			break;
     		
@@ -61,11 +68,11 @@ public class App {
     				
     				System.out.println("Please enter your username:");
     				
-    				//set username
+    				user.setUsername(Scan.input());
     				
     				System.out.println("Please enter your password:");
     				
-    				//Set password
+    				user.setPassword(Scan.input());
     			
     			break;
 
@@ -83,16 +90,16 @@ public class App {
     	 * Menus to be displayed after login
     	 */
     	
-    	));
+    	user = User.searchUsers(user.getUsername());
     	
     	int menuOptions = 0;
     	
-    	if() {
+    	if(user.getUsername().equals("root")) {
     		/*
     		 * If user has logged in as 'root' the root menu will be displayed
     		 */
     		menuOptions = 1;
-    	} else if() {
+    	} else if(user.getCustomerProfile().getCustomerID() == 0) {
     		/*
     		 * If user isn't a customer
     		 */
@@ -117,7 +124,7 @@ public class App {
 				System.out.println("Soaps - 5");
 				System.out.println("Exit - 0");
 
-				switch () {
+				switch (Controls.Selector()) {
 
 				case 0: // Exit
 					menuOptions = 0;
@@ -130,20 +137,42 @@ public class App {
 					System.out.println("Update User - 3");
 					System.out.println("Delete User - 4");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // Create
-						
+						User newUser = new User();
+						newUser = Controls.addNewUser(newUser);
 						break;
 					case 2: // Read
-						
+						ArrayList<Integer> userIDs = Controls.getIDs("user_id");
+						for (int ID : userIDs) {
+							System.out.println(User.searchUsers(ID));
+						}
 						break;
 					case 3: // Update
-						
+						System.out.println("Enter ID of user to update:");
+						int updUserID = Controls.Selector();
+						User updUser = User.searchUsers(updUserID);
+						System.out.println("Enter new username:");
+						updUser.setUsername(Scan.input());
+						System.out.println("Enter new password:");
+						updUser.setPassword(Scan.input());
+						System.out.println("Are you sure you want to change " + User.searchUsers(updUserID) + " to "
+								+ updUser + "? (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							updUser.updateUser(updUserID);
+							System.out.println("User successfully updated!");
+						}
 						break;
 					case 4: // Delete
-						
+						System.out.println("Enter ID of user to delete:");
+						int delUserID = Controls.Selector();
+						System.out.println("Are you sure you want to delete " + User.searchUsers(delUserID) + " (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							User.deleteUser(delUserID);
+							System.out.println("User deleted!");
+						}
 						break;
 					default:
 						System.out.println("Please enter a valid option!");
@@ -157,20 +186,43 @@ public class App {
 					System.out.println("Update Customer - 3");
 					System.out.println("Delete Customer - 4");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // Create
-						
+						Customer newCust = Controls.addNewCustomer();
+						System.out.println("Are you sure you want to add the following customer? (y/n)");
+						System.out.println(newCust);
+						if (Scan.input().equalsIgnoreCase("y")) {
+							newCust.addCustomer();
+							System.out.println("Customer added successfully!");
+						}
 						break;
 					case 2: // Read
-						
+						ArrayList<Integer> custIDs = Controls.getIDs("customer_id");
+						for (int ID : custIDs) {
+							System.out.println(Customer.search(ID));
+						}
 						break;
 					case 3: // Update
-						
+						System.out.println("Enter ID of customer to update:");
+						int updCustID = Controls.Selector();
+						Customer updCust = Controls.addNewCustomer();
+						System.out.println("Are you sure you want to change " + Customer.search(updCustID) + " to "
+								+ updCust + "? (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							updCust.updateCustomer(updCustID);
+							System.out.println("Customer successfully updated!");
+						}
 						break;
 					case 4: // Delete
-						
+						System.out.println("Enter ID of customer to delete:");
+						int delCustID = Controls.Selector();
+						System.out.println("Are you sure you want to delete " + Customer.search(delCustID) + " (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							Customer.deleteCustomer(delCustID);
+							System.out.println("Customer deleted!");
+						}
 						break;
 					default:
 						System.out.println("Please enter a valid option!");
@@ -184,24 +236,151 @@ public class App {
 					System.out.println("Update Order - 3");
 					System.out.println("Delete Order - 4");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // Create
-						
+						System.out.println("Enter ID of customer making this order:");
+						int custID = Controls.Selector();
+						Customer cust = Customer.search(custID);
+						cust.clearBasket();
+						boolean moreItems = false;
+						do {
+							System.out.println("Enter ID of item to order:");
+							int itemID = Controls.Selector();
+							System.out.println("Enter how many " + Item.searchItem(itemID).getItemName() + " to order:");
+							int quantity = Controls.Selector();
+							cust.addToBasket(itemID, quantity);
+							System.out.println("Do you want to continue adding items to this order? (y/n)");
+							if (Scan.input().equalsIgnoreCase("y")) {
+								moreItems = true;
+							} else {
+								moreItems = false;
+							}
+						} while (moreItems);
+						System.out.println("Do you want to place the order? (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							cust.placeOrder(cust.getBasket());
+							System.out.println("Order placed successfully!");
+						} else {
+							System.out.println("Order not placed!");
+						}
 						break;
 					case 2: // Read
-						
+						ArrayList<Integer> ordIDs = Controls.getIDs("order_id");
+						for (int ordID : ordIDs) {
+							Order order = Order.search(ordID);
+							System.out.println(order);
+						}
 						break;
 					case 3: // Update
-						//Back
-						// UUpdate Customer
-						//Update items
-							//Add Item
-							//Remove Item
-							// Change Quantity
-						//Delete
-						
+						System.out.println("Enter ID of order to update:");
+						int updOrderID = Controls.Selector();
+						Order updOrder = Order.search(updOrderID);
+						boolean cont = false;
+						do {
+							System.out.println("Change customer who made this order - 1");
+							System.out.println("Change order contents - 2");
+							System.out.println("Back - 0");
+							switch (Controls.Selector()) {
+							case 0: // Back
+								cont = false;
+								break;
+							case 1: // Update customer
+								System.out.println("Enter the ID of the Customer to assign this order to:");
+								int updCustID = Controls.Selector();
+								Customer updCust = Customer.search(updCustID);
+								System.out.println(
+										"Are you sure you want to reassign this order to " + updCust + "? (y/n)");
+								if (Scan.input().equalsIgnoreCase("y")) {
+									updOrder.setCustomer(updCust);
+									updOrder.update(updOrderID);
+									System.out.println("Customer successfully updated!");
+								}
+								cont = true;
+								break;
+							case 2: // Update Items
+								boolean moreUpdItems = false;
+								do {
+									List<Item> updItems = updOrder.getItems();
+									for (Item i : updItems) {
+										System.out.println(i);
+									}
+									System.out.println("Add Item - 1");
+									System.out.println("Remove Item - 2");
+									System.out.println("Change Quantity - 3");
+									System.out.println("Back - 0");
+									switch (Controls.Selector()) {
+									case 0: // Back
+										moreUpdItems = false;
+										break;
+									case 1: // Add Item
+										System.out.println("Enter ID of Item to Add:");
+										int newItemID = Controls.Selector();
+										Item newItem = Item.searchItem(newItemID);
+										System.out.println("How many " + newItem.getItemName() + "?:");
+										int quantity = Controls.Selector();
+										newItem.setItemStock(quantity);
+										updItems.add(newItem);
+										updOrder.setItems(updItems);
+										updOrder.update(updOrderID);
+										System.out.println("Added item successfully!");
+										moreUpdItems = true;
+										break;
+									case 2: // Remove Item
+										System.out.println("Enter ID of Item to Remove:");
+										int rmvItemID = Controls.Selector();
+										Item deleteMe = new Item();
+										for (Item i : updItems) {
+											if (i.getItemID() == rmvItemID) {
+												deleteMe = i;
+											}
+										}
+										updItems.remove(deleteMe);
+										updOrder.setItems(updItems);
+										updOrder.update(updOrderID);
+										System.out.println("Removed item successfully!");
+										moreUpdItems = true;
+										break;
+									case 3: // Change Quantity
+										System.out.println("Enter ID of Item to change quantity of:");
+										int updItemID = Controls.Selector();
+										System.out.println("Enter quantity to change to:");
+										int updQuantity = Controls.Selector();
+										for (Item i : updItems) {
+											if (i.getItemID() == updItemID) {
+												i.setItemStock(updQuantity);
+											}
+										}
+										updOrder.setItems(updItems);
+										updOrder.update(updOrderID);
+										System.out.println("Changed quantity successfully!");
+										moreUpdItems = true;
+										break;
+									}
+								} while (moreUpdItems);
+								cont = true;
+								break;
+							default:
+								System.out.println("Please enter a valid option!");
+								cont = true;
+								break;
+							}
+						} while (cont);
+						break;
+					case 4: // Delete
+						System.out.println("Enter ID of order to delete:");
+						int delOrderID = Controls.Selector();
+						System.out.println("Are you sure you want to delete " + Order.search(delOrderID) + " (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							Order.delete(delOrderID);
+							System.out.println("Order deleted!");
+						}
+						break;
+					default:
+						System.out.println("Please enter a valid option!");
+						break;
+					}
 					break;
 
 				case 4: // Items
@@ -210,20 +389,36 @@ public class App {
 					System.out.println("Update Item - 3");
 					System.out.println("Delete Item - 4");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // Create
-						
+						Item newItem = Controls.addNewItem();
+						if (Scan.input().equalsIgnoreCase("y")) {
+							newItem.add();
+							System.out.println("Item added successfully!");
+						}
 						break;
 					case 2: // Read
-						
+						Controls.browse();
 						break;
 					case 3: // Update
-						
+						System.out.println("Enter ID of Item to update:");
+						int updItemID = Controls.Selector();
+						Item updItem = Controls.addNewItem();
+						if (Scan.input().equalsIgnoreCase("y")) {
+							updItem.update(updItemID);
+							System.out.println("Item updated successfully!");
+						}
 						break;
 					case 4: // Delete
-						
+						System.out.println("Enter ID of Item to delete:");
+						int delItemID = Controls.Selector();
+						System.out.println("Are you sure you want to delete " + Item.searchItem(delItemID) + " (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							Item.deleteItem(delItemID);
+							System.out.println("Item deleted!");
+						}
 						break;
 					default:
 						System.out.println("Please enter a valid option!");
@@ -237,20 +432,39 @@ public class App {
 					System.out.println("Update Soap - 3");
 					System.out.println("Delete Soap - 4");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // Create
-						
+						Soap newSoap = Controls.addNewSoap();
+						if (Scan.input().equalsIgnoreCase("y")) {
+							newSoap.add();
+							System.out.println("Soap added successfully!");
+						}
 						break;
 					case 2: // Read
-						
+						ArrayList<Integer> IDs = Controls.getIDs("soap_id");
+						for (int ID : IDs) {
+							System.out.println(Soap.search(ID));
+						}
 						break;
 					case 3: // Update
-						
+						System.out.println("Enter ID of Soap to update:");
+						int updSoapID = Controls.Selector();
+						Soap updSoap = Controls.addNewSoap();
+						if (Scan.input().equalsIgnoreCase("y")) {
+							updSoap.update(updSoapID);
+							System.out.println("Soap updated successfully!");
+						}
 						break;
 					case 4: // Delete
-						
+						System.out.println("Enter ID of Soap to delete:");
+						int delSoapID = Controls.Selector();
+						System.out.println("Are you sure you want to delete " + Soap.search(delSoapID) + " (y/n)");
+						if (Scan.input().equalsIgnoreCase("y")) {
+							Soap.delete(delSoapID);
+							System.out.println("Soap deleted!");
+						}
 						break;
 					default:
 						System.out.println("Please enter a valid option!");
@@ -272,7 +486,7 @@ public class App {
 				System.out.println("My Account - 3");
 				System.out.println("Exit - 0");
 
-				switch () {
+				switch (Controls.Selector()) {
 
 				case 0: // Exit
 					menuOptions = 0;
@@ -292,7 +506,7 @@ public class App {
 					System.out.println("Change Username - 2");
 					System.out.println("Change Password - 3");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // View Profile
@@ -324,7 +538,7 @@ public class App {
 				System.out.println("My Account - 3");
 				System.out.println("Exit - 0");
 
-				switch () {
+				switch (Controls.Selector()) {
 
 				case 0: // Exit
 					menuOptions = 0;
@@ -342,7 +556,7 @@ public class App {
 					System.out.println("Change Quantity - 4");
 					System.out.println("Checkout - 5");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // View Basket
@@ -373,7 +587,7 @@ public class App {
 					System.out.println("Change Password - 4");
 					System.out.println("Update Profile - 5");
 					System.out.println("Back - 0");
-					switch () {
+					switch (Controls.Selector()) {
 					case 0: // Back
 						break;
 					case 1: // View Orders
@@ -406,7 +620,8 @@ public class App {
 				break;
 
 			default:
-				
+				System.out.println("Unknown user!");
+				menuOptions = 0;
 				break;
 			}
 
